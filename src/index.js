75893f4921a89a878195ce1c48c3fd71633718ca
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2019-12-12 22:19:14 +0300

    refactor(loader): reorganize main loader code
    move all the logic to compiler

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
 import { getOptions } from 'loader-utils';
 import validateOptions from 'schema-utils';
 
+import SwaggerParser from 'swagger-parser';
+import { format } from 'json-schema-to-typescript/dist/src/formatter';
+
+import { defaultOptions } from './utils/schema2typescript';
+
+import { compileModels, compileRuntime, compileApis } from './compile';
 import schema from './options.json';
 
 export const raw = true;
 
-export default function loader(source) {
-  const { version, webpack } = this;
+export default function loader() {
+  const callback = this.async();
 
   const options = getOptions(this) || {};
 
   validateOptions(schema, options, 'Loader');
 
-  const newSource = `
-  /**
-   * Loader API Version: ${version}
-   * Is this in "webpack mode": ${webpack}
-   */
-  /**
-   * Original Source From Loader
-   */
-  ${source}`;
-
-  return `${newSource}`;
+  SwaggerParser.parse(this.resourcePath, (err, api) =>
+    Promise.all([
+      compileModels(api, options),
+      compileRuntime(api, options),
+      compileApis(api, options),
+    ]).then((results) => {
+      callback(null, format(results.join('\n'), defaultOptions));
+    })
+  ).catch((e) => callback(e, null));
 }

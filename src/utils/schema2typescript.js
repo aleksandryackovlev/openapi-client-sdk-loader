997c1a18373ed27a137b549ae21f771f0fee205f
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2019-12-12 22:16:16 +0300

    feat(utils): add json schema to typescript compiler

---
+++ src/utils/schema2typescript.js
@@ -0,0 +1,91 @@
+import { endsWith, cloneDeep } from 'lodash';
+import { format } from 'json-schema-to-typescript/dist/src/formatter';
+import { generate } from 'json-schema-to-typescript/dist/src/generator';
+import { normalize } from 'json-schema-to-typescript/dist/src/normalizer';
+import { optimize } from 'json-schema-to-typescript/dist/src/optimizer';
+import { parse } from 'json-schema-to-typescript/dist/src/parser';
+import { dereference } from 'json-schema-to-typescript/dist/src/resolver';
+import { validate } from 'json-schema-to-typescript/dist/src/validator';
+import { traverse } from 'json-schema-to-typescript/dist/src/utils';
+import { ValidationError } from 'json-schema-to-typescript';
+
+export const defaultOptions = {
+  $refOptions: {},
+  bannerComment: '',
+  cwd: process.cwd(),
+  declareExternallyReferenced: true,
+  enableConstEnums: true,
+  strictIndexSignatures: false,
+  style: {
+    bracketSpacing: false,
+    printWidth: 120,
+    semi: true,
+    singleQuote: false,
+    tabWidth: 2,
+    trailingComma: 'none',
+    useTabs: false,
+  },
+  unreachableDefinitions: false,
+};
+
+/* eslint-disable no-param-reassign */
+function hasType(schema, type) {
+  return (
+    schema.type === type ||
+    (Array.isArray(schema.type) && schema.type.includes(type))
+  );
+}
+function isObjectType(schema) {
+  return typeof schema.properties !== 'undefined' || hasType(schema, 'object');
+}
+
+function rule(schema) {
+  if (schema.$ref && schema.$ref.includes('components/')) {
+    const pathFragments = schema.$ref.split('/');
+    delete schema.$ref;
+    schema.tsType = pathFragments[pathFragments.length - 1];
+  }
+
+  if (!('additionalProperties' in schema) && isObjectType(schema)) {
+    schema.additionalProperties = false;
+  }
+}
+
+function normalizeRefs(schema, filename) {
+  const clonedSchema = cloneDeep(schema);
+
+  traverse(clonedSchema, (schemaToConvert) =>
+    rule(schemaToConvert, clonedSchema, filename)
+  );
+
+  return clonedSchema;
+}
+
+async function compile(schema, name, options = defaultOptions) {
+  const errors = validate(schema, name);
+
+  if (errors.length) {
+    // errors.forEach(_ => error(_));
+    throw new ValidationError();
+  }
+
+  if (!endsWith(options.cwd, '/')) {
+    options.cwd += '/';
+  }
+
+  return format(
+    generate(
+      optimize(
+        parse(
+          await dereference(normalize(normalizeRefs(schema), name), options),
+          options
+        )
+      ),
+      options
+    ),
+    options
+  );
+}
+/* eslint-enable no-param-reassign */
+
+export default compile;

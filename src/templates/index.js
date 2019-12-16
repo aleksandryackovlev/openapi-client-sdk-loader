a3fa16ea13cdacb9a463dde83e93c5a9e8fa652c
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2019-12-16 22:37:41 +0300

    feat(templates): add handlebars templates for source code compilation

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,1 +1,55 @@
-export default (source) => source;
+import fs from 'fs';
+import path from 'path';
+
+import Handlebars from 'handlebars';
+import glob from 'glob';
+
+export default (source, templatePath) => {
+  const template = fs.readFileSync(`${templatePath}/index.handlebars`);
+
+  Handlebars.registerHelper({
+    json(context) {
+      return JSON.stringify(context);
+    },
+    eq(v1, v2) {
+      return v1 === v2;
+    },
+    ne(v1, v2) {
+      return v1 !== v2;
+    },
+    lt(v1, v2) {
+      return v1 < v2;
+    },
+    gt(v1, v2) {
+      return v1 > v2;
+    },
+    lte(v1, v2) {
+      return v1 <= v2;
+    },
+    gte(v1, v2) {
+      return v1 >= v2;
+    },
+    and(...args) {
+      return args.every(Boolean);
+    },
+    or(...args) {
+      return args.slice(0, -1).some(Boolean);
+    },
+    not(context) {
+      return !!context;
+    },
+  });
+
+  const partials = glob.sync(`${templatePath}/**/*.handlebars`);
+  if (partials && partials.length) {
+    partials.forEach((partial) => {
+      if (partial !== `${templatePath}/index.handlebars`) {
+        Handlebars.registerPartial(path.parse(partial).name, fs.readFileSync(partial).toString());
+      }
+    });
+  }
+
+  const compiled = Handlebars.compile(template.toString(), { noEscape: true });
+
+  return compiled(source);
+};

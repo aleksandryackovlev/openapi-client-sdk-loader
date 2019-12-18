86f405f9eba01f720dc60383a70e4b2d08bba58c
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2019-12-18 21:40:58 +0300

    feat(compile): compile server info in the ts compiler

--- src/compile/ts/index.js
+++ src/compile/ts/index.js
@@ -1,11 +1,20 @@
 import compileModels from './compileModels';
 import compileOperations from './compileOperations';
 
+const compileInfo = (api) => {
+  return Promise.resolve({
+    server: api.servers ? api.servers[0].url : '',
+  });
+};
+
 export default (api, options) => {
-  return Promise.all([compileModels(api, options), compileOperations(api, options)]).then(
-    ([models, operations]) => ({
-      models,
-      operations,
-    })
-  );
+  return Promise.all([
+    compileModels(api, options),
+    compileOperations(api, options),
+    compileInfo(api, options),
+  ]).then(([models, operations, info]) => ({
+    models,
+    operations,
+    info,
+  }));
 };

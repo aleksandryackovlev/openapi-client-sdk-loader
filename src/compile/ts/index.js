c3b97f1ecabee6a257d74249db1709cb3e2e79fa
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2019-12-19 22:00:53 +0300

    refactor(compile): move server info from the async func to the final compilation prop

--- src/compile/ts/index.js
+++ src/compile/ts/index.js
@@ -1,20 +1,12 @@
 import compileModels from './compileModels';
 import compileOperations from './compileOperations';
 
-const compileInfo = (api) => {
-  return Promise.resolve({
-    server: api.servers ? api.servers[0].url : '',
-  });
-};
-
 export default (api, options) => {
-  return Promise.all([
-    compileModels(api, options),
-    compileOperations(api, options),
-    compileInfo(api, options),
-  ]).then(([models, operations, info]) => ({
-    models,
-    operations,
-    info,
-  }));
+  return Promise.all([compileModels(api, options), compileOperations(api, options)]).then(
+    ([models, operations]) => ({
+      models,
+      operations,
+      info: api.servers ? api.servers[0].url : '',
+    })
+  );
 };

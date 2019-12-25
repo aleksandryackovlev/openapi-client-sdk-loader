9c36b0fdf1625e5e7551ea74c9065d97dfa5ff19
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2019-12-25 17:47:36 +0300

    fix(compile): fix models' compilation when the components section is not described the doc

--- src/compile/ts/compileModels.js
+++ src/compile/ts/compileModels.js
@@ -23,8 +23,19 @@ const compileBodies = (bodies, nameSuffix) =>
 
 export default async (api) =>
   Promise.all([
-    compile(api.components.schemas, (schemas, name) => [schemas[name], name]),
-    compile(api.components.parameters, (schemas, name) => [schemas[name].schema, `${name}Param`]),
-    ...compileBodies(api.components.requestBodies, 'RequestBody'),
-    ...compileBodies(api.components.responses, 'Response'),
+    api.components && api.components.shemas
+      ? compile(api.components.schemas, (schemas, name) => [schemas[name], name])
+      : Promise.resolve(''),
+    api.components && api.components.shemas
+      ? compile(api.components.parameters, (schemas, name) => [
+          schemas[name].schema,
+          `${name}Param`,
+        ])
+      : Promise.resolve(''),
+    ...(api.components && api.components.requestBodies
+      ? compileBodies(api.components.requestBodies, 'RequestBody')
+      : []),
+    ...(api.components && api.components.responses
+      ? compileBodies(api.components.responses, 'Response')
+      : []),
   ]).then((interfaces) => interfaces.join('\n'));

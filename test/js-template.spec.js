d3ba67fca3f6275d622b2cfad6ed36a0c183e03a
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2020-01-06 14:15:45 +0300

    test(js-template): setup tests for the js-template generated code

---
+++ test/js-template.spec.js
@@ -0,0 +1,43 @@
+describe('js-template', () => {
+  it('should send a request with correct params', async () => {});
+
+  it('should throw on incorrect params if it was compiled with validateRequest flag', async () => {});
+
+  it('should send a request with correct query string', async () => {});
+
+  it('should throw on incorrect query string if it was compiled with validateRequest flag', async () => {});
+
+  it('should send a request with correct body', async () => {});
+
+  it('should send files without errors', async () => {});
+
+  it('should throw on incorrect FormData body', async () => {});
+
+  it('should throw on incorrect body if it was compiled with validateRequest flag', async () => {});
+
+  it('should send a request with correct headers', async () => {});
+
+  it('should throw on incorrect headers if it was compiled with validateRequest flag', async () => {});
+
+  it('should send a request with correct http method', async () => {});
+
+  it('should send a request with to the url specified in docs', async () => {});
+
+  it('should send a request with to the url specified the environment', async () => {});
+
+  it('should execute pre middleware before the request if it is set', async () => {});
+
+  it('should return the result specified in the docs', async () => {});
+
+  it('should throw on incorrect result if it was compiled with validateResponse flag', async () => {});
+
+  it('should choose application/json request content type, if there are more than one given', async () => {});
+
+  it('should throw on unsupported request content-types', async () => {});
+
+  it('should throw on unsupported response content-types', async () => {});
+
+  it('should throw with an ApiError if response status is not ok', async () => {});
+
+  it('should throw with an ClientError if something goes wrong during code execution', async () => {});
+});

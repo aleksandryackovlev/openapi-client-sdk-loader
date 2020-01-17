7e3871d859805216a25c919e36cb93c560586c02
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2020-01-17 22:56:50 +0300

    feat(js-template): handle multiply types of responses

--- src/compile/js/compileOperations.js
+++ src/compile/js/compileOperations.js
@@ -1,31 +1,6 @@
 import { cloneDeep } from 'lodash';
 
-import { basepath, dashes2capitals } from '../../utils';
-
-const prepareMimeType = (mimeType) => dashes2capitals(basepath(mimeType.split(';')[0]));
-
-const getParams = (schemas) =>
-  schemas.reduce((prevParams, { name, in: inPath, required, schema: paramSchema }) => {
-    const inName = inPath === 'path' ? 'params' : inPath;
-    const params = { ...prevParams };
-
-    if (!params[inName]) {
-      params[inName] = {
-        type: 'object',
-        required: [],
-        properties: {},
-        additionalProperties: false,
-      };
-    }
-
-    params[inName].properties[name] = paramSchema;
-
-    if (required) {
-      params[inName].required.push(name);
-    }
-
-    return params;
-  }, {});
+import { parseParams, parseBody, parseResponses } from '../utils';
 
 const createOperations = (api, deref) =>
   Promise.all(
@@ -35,9 +10,6 @@ const createOperations = (api, deref) =>
         ...Object.keys(deref.paths[path]).map(async (method) => {
           const operation = deref.paths[path][method];
 
-          const globalSecurity = api.security;
-          const localSecurity = operation.security;
-
           let schema = {
             method: method.toUpperCase(),
             path,
@@ -46,78 +18,15 @@ const createOperations = (api, deref) =>
           };
 
           if (operation.parameters) {
-            const params = getParams(operation.parameters);
-            schema = { ...schema, ...params };
-          }
-
-          schema.securityScheme = null;
-
-          if (globalSecurity || localSecurity) {
-            if (localSecurity) {
-              schema.securityScheme = localSecurity
-                .map((security) => Object.keys(security)[0])
-                .map(
-                  (securitySchema) =>
-                    api.components &&
-                    api.components.securitySchemes && {
-                      name: securitySchema,
-                      ...api.components.securitySchemes[securitySchema],
-                    }
-                )
-                .filter(Boolean);
-            } else {
-              schema.securityScheme = globalSecurity
-                .map((security) => Object.keys(security)[0])
-                .map(
-                  (securitySchema) =>
-                    api.components &&
-                    api.components.securitySchemes && {
-                      name: securitySchema,
-                      ...api.components.securitySchemes[securitySchema],
-                    }
-                )
-                .filter(Boolean);
-            }
+            schema = { ...schema, ...parseParams(operation.parameters) };
           }
 
           if (operation.requestBody) {
-            const requestBody = operation.requestBody.content;
-
-            // TODO: Support multiple Content-Types
-            if (
-              Object.keys(requestBody).some((mimeType) => mimeType.startsWith('application/json'))
-            ) {
-              const mimeType = Object.keys(requestBody).find((bodyMimeType) =>
-                bodyMimeType.startsWith('application/json')
-              );
-
-              schema.mimeType = mimeType;
-              schema.mimeTypeSuffix = prepareMimeType(mimeType);
-              schema.data = requestBody[mimeType].schema;
-            } else if (Object.keys(requestBody).length) {
-              const mimeType = Object.keys(requestBody)[0];
-
-              schema.mimeType = mimeType;
-              schema.mimeTypeSuffix = prepareMimeType(mimeType);
-              schema.data = requestBody[mimeType].schema;
-            }
+            schema = { ...schema, ...parseBody(operation.requestBody.content) };
           }
 
-          // Support only 200 responses with the Content-Type application/json
-          if (operation.responses && (operation.responses['200'] || operation.responses['201'])) {
-            if (
-              operation.responses['200'] &&
-              operation.responses['200'].content &&
-              operation.responses['200'].content['application/json']
-            ) {
-              schema.response = operation.responses['200'].content['application/json'].schema;
-            } else if (
-              operation.responses['201'] &&
-              operation.responses['201'].content &&
-              operation.responses['201'].content['application/json']
-            ) {
-              schema.response = operation.responses['201'].content['application/json'].schema;
-            }
+          if (operation.responses) {
+            schema = { ...schema, ...parseResponses(operation.responses) };
           }
 
           return schema;
